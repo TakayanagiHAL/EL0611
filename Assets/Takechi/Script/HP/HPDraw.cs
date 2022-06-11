0aa1331aacd7b25dcbc1da7a90edb1c36e4c4493
Programmer-7501 <gameprogrammerforest10@gmail.com>
2022-06-11 15:27:09 +0900

    HPDraw更新

--- Assets/Takechi/Script/HP/HPDraw.cs
+++ Assets/Takechi/Script/HP/HPDraw.cs
@@ -7,6 +7,8 @@ public class HPDraw : MonoBehaviour
     [SerializeField]
     GameObject[] Heart = new GameObject[5];
 
+    int a = 5;
+
     void Start()
     {
         for(int i = 0; i < 5; i++)
@@ -17,6 +19,38 @@ public class HPDraw : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        switch (a)
+        {
+            case 4:
+                Heart[4].SetActive(false);
+                break;
+            case 3:
+                Heart[4].SetActive(false);
+                Heart[3].SetActive(false);
+                break;
+            case 2:
+                Heart[4].SetActive(false);
+                Heart[3].SetActive(false);
+                Heart[2].SetActive(false);
+                break;
+            case 1:
+                Heart[4].SetActive(false);
+                Heart[3].SetActive(false);
+                Heart[2].SetActive(false);
+                Heart[1].SetActive(false);
+                break;
+            case 0:
+                Heart[4].SetActive(false);
+                Heart[3].SetActive(false);
+                Heart[2].SetActive(false);
+                Heart[1].SetActive(false);
+                Heart[0].SetActive(false);
+                break;
+        }
+       
+        if(Input.GetKey(KeyCode.R))
+        {
+            a = 4;
+        }
     }
 }
