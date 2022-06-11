014f60e652cd340ad3489c57194f65890f41d972
TakayanagiHAL <takayanagi.hal.at@gmail.com>
2022-06-11 16:58:25 +0900

    修正

--- Assets/takayanagi/BGMPlay.cs
+++ Assets/takayanagi/BGMPlay.cs
@@ -1,22 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class MaxScoreDraw : MonoBehaviour
+public class BGMPlay : MonoBehaviour
 {
-    [SerializeField]
-    private Text ScoreText;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        SoundManager.instance.PlayBGM("ゲーム");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //ScoreText.text = "最大スコア:" + ScoreManager.GetCurrentScore().ToString();
+        
     }
 }
